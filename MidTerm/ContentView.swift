2997e73a999ca404dcbe4dc7ac6724cf5d4f1c13
Thomas Murphy <foxsniper11@gmail.com>
2022-03-22 04:12:19 -0400

    Initial Commit

---
+++ MidTerm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MidTerm
+//
+//  Created by Thomas Murphy on 3/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MidTerm/MidTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidTermApp.swift
+//  MidTerm
+//
+//  Created by Thomas Murphy on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MidTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
